e86e9816e2a5e04f398b9978af5c8dd7773fff46
vaha77 <abdulvohid996@gmail.com>
2022-04-09 00:21:03 +0900

    몸

---
+++ src/Data/index.js
@@ -0,0 +1,15 @@
+import burger from "../assets/imgs/chizBurger.png";
+
+export const pizza = [
+  {
+    id: 1,
+    title: "Чизбургер-пицца",
+    img: burger,
+    price: "от 395 ₽",
+    desc: "тонкое",
+    desc2: "традиционное",
+    size1: "26 см.",
+    size2: "30 см.",
+    size3: "40 см.",
+  },
+];
